43838b6d06095c92c3ecb5ed483f99eb5e52319f
usnaik <upendra_naik@yahoo.com>
2020-05-15 12:44:06 +0530

    cleanup

--- full-stack-proj/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-	// res.send('Hello world');
-	res.render('index');
-});
-
-module.exports = router;
